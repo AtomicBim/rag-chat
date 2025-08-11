28f29aefadb5679afdb2ecc991015dab8f296ba9
AtomicBim <romangman90@gmail.com>
2025-08-11 11:04:18 +0500

    в прод

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
-QDRANT_HOST = "qdrant"  # Qdrant работает на той же машине, можно использовать localhost
+QDRANT_HOST = "192.168.42.188"
 QDRANT_PORT = 6333
 COLLECTION_NAME = "internal_regulations_v2"
-SEARCH_LIMIT = 5 # Лимит для более  релевантных ответов
+SEARCH_LIMIT = 5 # Лимит документов для проверки ответа
 
-# IP вашего ПК с GPU, где запущен embedding_service.py
 EMBEDDING_SERVICE_ENDPOINT = "http://192.168.45.55:8001/create_embedding" 
-# IP вашей ВМ, где запущен ask_question.py
 OPENAI_API_ENDPOINT = "http://192.168.45.79:8000/generate_answer" 
